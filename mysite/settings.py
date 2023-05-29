ac7261b7ff287c376330d5989efe0016d05d7bdb
xu.z.brandon@gmail.com <68095440+bxrr@users.noreply.github.com>
2023-05-29 14:30:50 -0700

    potential bug fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-446q$a@!jujtlt&d8wliz5+(0gx96c@zhf!5!+64@%-vu*4zw7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
